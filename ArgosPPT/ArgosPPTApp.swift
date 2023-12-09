0d28ba82eac40bded50e3d02e05b728c017b077d
Mo Abdo <moabdo1212@gmail.com>
2023-12-08 19:21:12 -0500

    Initial Commit

---
+++ ArgosPPT/ArgosPPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArgosPPTApp.swift
+//  ArgosPPT
+//
+//  Created by Mo Abdo on 12/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ArgosPPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
